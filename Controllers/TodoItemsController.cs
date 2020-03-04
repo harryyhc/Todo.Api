a860dd3c90c7d6fbcd6164db59827f5875df3f2e
Harry You <harryyhc@outlook.com>
2020-03-04 07:47:47 -0500

    2nd Commit

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Todo.Api.Models;
+using Todo.Api.Services;
+
+namespace Todo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/todos")]
+    public class TodoItemsController : ControllerBase
+    {
+        private ITodoRepository repository;
+        public TodoItemsController(ITodoRepository todoRepository)
+        {
+            repository = todoRepository ??
+                throw new ArgumentNullException(nameof(todoRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            var result = repository.GetAllTodoItems();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+
+}
